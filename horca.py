084021a74380ab2bc2b9a2d99ee249484a5498cc
AdrianPC <adriancamilopc@gmail.com>
2023-11-15 22:58:22 -0500

    file game and horca

---
+++ horca.py
@@ -0,0 +1,6 @@
+print("************************************")
+print("    Juego de la Horca - Bienvenido   ")
+print("************************************")
+
+print("Elige el nivel de dificultad")
+print("(1) Novato (2) Intermedio (3) Avanzado")
---
+++ juegos.py
@@ -0,0 +1,16 @@
+"""import games"""
+import horca
+import adivina_numero
+
+
+print("************************************")
+print("    Juegos de Python   ")
+print("************************************")
+
+juego = int(input("Selecciona el juego que deseas"))
+print("(1) Horca (2) Adivinanza")
+if juego == 1:
+    print("Juego de la horca")
+elif juego == 2:
+    print("Juego adivina el numero")
+
