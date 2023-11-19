58405e79bdcc5dc787fd5895d709c1e99d134c73
AdrianPC <adriancamilopc@gmail.com>
2023-11-18 22:54:50 -0500

    adding new words secrets

--- horca.py
+++ horca.py
@@ -8,29 +8,41 @@ def jugar():
     print("Elige el nivel de dificultad")
     print("(1) Novato (2) Intermedio (3) Avanzado")
 
-palabra_secreta = "banana"
+palabra_secreta = "fresa".upper()
 palabra_secreta = palabra_secreta.strip()
-letras_acertadas = ['_','_','_','_','_','_']
+letras_acertadas = ['_' for letra in palabra_secreta]
 print(letras_acertadas)
 
 # Mientras la persona no se ahorque ni acierte la palabra el juego continua
 ahorco = False
 acerto = False
+errores = 0
 
 while (not ahorco and not acerto):
     intento = input("\nDigita una letra: ")
-    intento = intento.strip()
-    indice = 0
-    for letra in palabra_secreta:
-        if intento.upper() == letra.upper():
-            letras_acertadas[indice] = letra.upper()
-            #print("Encontre la letra '{letra}' en la posicion {indice}")
-        indice = indice + 1
+    intento = intento.strip().upper()
+    if intento in palabra_secreta:
+        indice = 0
+
+        for letra in palabra_secreta:
+            if intento == letra:
+                letras_acertadas[indice] = letra
+                #print("Encontre la letra '{letra}' en la posicion {indice}")
+            indice = indice + 1
+    else:
+        errores += 1
+        #print("¡Ups, has fallado! Quedan {} intentos.".format(6-errores))
+    ahorco = errores == 6
+    acerto = '_' not in letras_acertadas
     print(letras_acertadas)
+    if ahorco:
+        print("\nPerdiste el juego")
+    elif acerto:
+        print("\nGanaste el juego")
 
 
 
-print("Fin del juego")
+print("\nFin del juego")
 
 
 if (__name__=="__main__"):
