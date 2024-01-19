3b60d04c9c56d4614491fe102d62823234564338
AdrianPC <adriancamilopc@gmail.com>
2024-01-18 23:33:21 -0500

    first creatin account bank for practice

--- POO.py
+++ POO.py
@@ -17,3 +17,21 @@ cuenta(321, 'Jose', 100.0, 1000.0)
 
 cuenta_1 = cuenta(321, 'Jose', 100.0, 1000.0)
 cuenta_1['numero']
+
+
+#retirar dinero cuenta
+def retira(cuenta,valor):
+    cuenta['saldo'] -= valor
+
+def deposita(cuenta,valor):
+    cuenta['saldo'] += valor
+
+def extracto(cuenta):
+    print(f"El saldo de la cuenta de {cuenta['titular']} es: {cuenta['saldo']}")
+
+retira(cuenta_1,40)
+extracto(cuenta_1)
+
+deposita(cuenta_1, 100)
+extracto(cuenta_1)
+
